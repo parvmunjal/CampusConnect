951c420cb3ddb91fa9bf7a3e7c838caa8ea41e12
parvmunjal <parvmunjal100@gmail.com>
2025-04-09 16:58:24 +0530

    Empty commit

--- src/main/java/com/cc/controller/EventController.java
+++ src/main/java/com/cc/controller/EventController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//Event controller
 @RestController
 @RequestMapping("/events")
 @CrossOrigin(origins = "http://127.0.0.1:5500")
